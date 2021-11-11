e773e4b125d2467888bb26f7a772edfd372394d8
LAPTOP-Q6H16585\Angel Giraldo <maggavaromp@gmail.com>
2021-11-11 18:59:24 -0500

    Lo logramos muchachxs

---
+++ Golotodo/app/src/main/java/com/unicaldas/golotodo/LOGIN.java
@@ -0,0 +1,14 @@
+package com.unicaldas.golotodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LOGIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
