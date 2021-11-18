cfce939b409557bf169b117022c82a3ee92a7b7d
jeimi romero <jeimi.romero@gmail.com>
2021-11-18 16:55:27 -0500

    se pasaron los archivos de la rama main a la rama jeimi

--- Golotodo/app/src/main/java/com/unicaldas/golotodo/LOGIN.java
+++ Golotodo/app/src/main/java/com/unicaldas/golotodo/LOGIN.java
@@ -2,13 +2,56 @@ package com.unicaldas.golotodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LOGIN extends AppCompatActivity {
+public class LOGIN extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_register;
+    private Button btn_login;
+    private EditText edt_username;
+    private EditText edt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_register = findViewById(R.id.btn_register);
+        btn_login = findViewById(R.id.btn_login);
+        edt_username = findViewById(R.id.edt_username);
+        edt_password = findViewById(R.id.edt_password);
+
+        btn_register.setOnClickListener(this);
+        btn_login.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+                String email = edt_username.getText().toString();
+                String password = edt_password.getText().toString();
+                if(email.equals("golotodo@email.com") && password.equals("grupo2")){
+                    Toast.makeText(this,getString(R.string.txt_LoginAction), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this,DrawerActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,getString(R.string.txt_LoginError), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btn_register:
+                Toast.makeText(this, getString(R.string.txt_registerWill), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this,Register.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
--- Golotodo/app/src/main/java/com/unicaldas/golotodo/Register.java
+++ Golotodo/app/src/main/java/com/unicaldas/golotodo/Register.java
@@ -3,12 +3,47 @@ package com.unicaldas.golotodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private CheckBox checkbox;
+    private Button registerFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        checkbox = findViewById(R.id.checkbox);
+        registerFinish = findViewById(R.id.registerFinish);
+
+        registerFinish.setEnabled(false);
+
+        checkbox.setOnClickListener(this);
+        registerFinish.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerFinish:
+                Toast.makeText(this, getString(R.string.txt_registerAction), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.checkbox:
+                if(checkbox.isChecked()){
+                    registerFinish.setEnabled(true);
+                    Toast.makeText(this, getString(R.string.txt_Enable), Toast.LENGTH_SHORT).show();
+                }else{
+                    registerFinish.setEnabled(false);
+                    Toast.makeText(this, getString(R.string.txt_Disable), Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+
     }
 }
