bc40bc660c6c80ec72664bcfb88d67441c4d150e
jeimi romero <jeimi.romero@gmail.com>
2021-11-20 22:07:00 -0500

    se realiza la vista de terminos y condiciones, falta hacer vinculo de botones desde el checkbox y hacia el checkbox para registrar

---
+++ Golotodo/app/src/main/java/com/unicaldas/golotodo/TerminosActivity.java
@@ -0,0 +1,25 @@
+package com.unicaldas.golotodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+
+
+public class TerminosActivity extends AppCompatActivity {
+    TextView textView;
+    WebView webView;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminos);
+    }
+
+}
