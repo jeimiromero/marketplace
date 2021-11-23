bb3409b4cbef615c823b8aec890eb8116f80141f
jeimi romero <jeimi.romero@gmail.com>
2021-11-22 20:46:11 -0500

    actividad funcional de registro

--- Golotodo/app/src/main/java/com/unicaldas/golotodo/Register.java
+++ Golotodo/app/src/main/java/com/unicaldas/golotodo/Register.java
@@ -1,49 +1,87 @@
 package com.unicaldas.golotodo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
 
-    private CheckBox checkbox;
-    private Button registerFinish;
+    private CheckBox chb_terminos;
+    private TextView tev_terminos;
+    private Button btn_registerFinish;
+    private final int ACTIVIDAD_TERMINOS=1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        checkbox = findViewById(R.id.checkbox);
-        registerFinish = findViewById(R.id.registerFinish);
+        chb_terminos = findViewById(R.id.chb_terminos);
+        btn_registerFinish = findViewById(R.id.registerFinish);
+        tev_terminos = findViewById(R.id.tev_terminos);
 
-        registerFinish.setEnabled(false);
+        //registerFinish.setEnabled(false);
 
-        checkbox.setOnClickListener(this);
-        registerFinish.setOnClickListener(this);
+
+        btn_registerFinish.setOnClickListener(this);
+        tev_terminos.setOnClickListener(this);
+
+        chb_terminos.setEnabled(false);
+
+        chb_terminos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                btn_registerFinish.setEnabled(isChecked);
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.registerFinish:
                 Toast.makeText(this, getString(R.string.txt_registerAction), Toast.LENGTH_SHORT).show();
+                Intent intent_register= new Intent(this,LOGIN.class);
+                startActivity(intent_register);
                 break;
-            case R.id.checkbox:
-                if(checkbox.isChecked()){
-                    registerFinish.setEnabled(true);
-                    Toast.makeText(this, getString(R.string.txt_Enable), Toast.LENGTH_SHORT).show();
-                }else{
-                    registerFinish.setEnabled(false);
-                    Toast.makeText(this, getString(R.string.txt_Disable), Toast.LENGTH_SHORT).show();
-                }
+
+            case R.id.tev_terminos:
+                Toast.makeText(this, getString(R.string.txt_registerAction), Toast.LENGTH_SHORT).show();
+                Intent intent_terminos= new Intent(this,TerminosActivity.class);
+                //startActivity(intent_terminos);
+                startActivityForResult(intent_terminos,ACTIVIDAD_TERMINOS);
                 break;
+
+
+
 
         }
 
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTIVIDAD_TERMINOS) {
+            if(resultCode== Activity.RESULT_OK){
+                chb_terminos.setChecked(true);
+
+            }
+            else {
+                chb_terminos.setChecked(false);
+            }
+        }
     }
 }
--- Golotodo/app/src/main/java/com/unicaldas/golotodo/TerminosActivity.java
+++ Golotodo/app/src/main/java/com/unicaldas/golotodo/TerminosActivity.java
@@ -1,25 +1,41 @@
 package com.unicaldas.golotodo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.widget.TextView;
-
-
-
 public class TerminosActivity extends AppCompatActivity {
-    TextView textView;
-    WebView webView;
 
-
-
-
+    private Button btn_registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terminos);
+
+        btn_registro = findViewById(R.id.btn_registro);
+
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+
+        }
+    }
 }
